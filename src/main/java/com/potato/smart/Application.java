2a5aa57ccc070bd9c5cc09534b42b0c2268bd81e
dehuab <Tujiascm333>
2019-01-12 22:48:17 +0800

    hello spring boot

---
+++ src/main/java/com/potato/smart/Application.java
@@ -0,0 +1,23 @@
+package com.potato.smart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 约定优先配置
+ * <p>
+ * 在SpringApplication.run()方法执行后，Spring Boot的autoconfigure发现这是一个Web应用（根据类路径上的依赖确定），
+ * 于是在内嵌的Tomcat容器中启动了一个Spring的应用上下文，并且监听默认的tcp端口8080（默认约定）。
+ * 同时在Spring Context中根据默认的约定配置了Spring WebMvc： *
+ * Servlet容器默认的Context路径是/
+ * DispatcherServlet匹配的路径(servlet-mapping中的url-patterns)是/*
+ * ComponentScan路径被默认设置为SampleController的同名package，也就是该package下的所有@Controller *
+ * Service, Component, Repository都会被实例化后并加入Spring Context中。
+ * </p>
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/main/java/com/potato/smart/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.potato.smart.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String helloSpringBoot() {
+        return "Hello SpringBoot!";
+    }
+}
